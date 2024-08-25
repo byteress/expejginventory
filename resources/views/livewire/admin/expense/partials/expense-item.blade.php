75a9383539fe98af23dc001f661ed3fb27e139b5
stefuuuuu <stephan.sutter2000@gmail.com>
2024-08-25 11:22:57 +0800

    Update

--- resources/views/livewire/admin/expense/partials/expense-item.blade.php
+++ resources/views/livewire/admin/expense/partials/expense-item.blade.php
@@ -1,5 +1,4 @@
 <tr x-data="{ edit: false }">
-    <form wire:submit="submit">
     <td><span x-show="!edit">{{ $this->getExpenseName($expense) }}</span> <select  x-show="edit" wire:model="expense" class="form-control" id="expense_type">
             @foreach($expenses as $expense)
                 <option value="{{ $expense->value }}">{{ $expense->displayName() }}</option>
@@ -7,7 +6,7 @@
         </select>
     </td>
     <td><span x-show="!edit">@money($amount, 'PHP', true)</span> <input wire:model="amount" x-show="edit" type="number" min="0.01" step="0.01" required class = "form-control" ></td>
-    <td><span x-show="!edit">{{ $description }}</span> <textarea x-show="edit" wire:model="description" class="form-control" id="details" rows="3" style = "resize:none;"></textarea></td>
+    <td><span x-show="!edit">{{ $description }}</span> <textarea x-show="edit" wire:model="description" class="form-control" id="details" rows="3"></textarea></td>
     @hasrole('admin')
     <td><span>{{ $this->getBranchName($branchId) }}</span></td>
     @endhasrole
@@ -19,12 +18,11 @@
             <i class="fa fa-trash"></i>
         </a>
 
-        <button x-show="edit" x-on:click="edit = false" type="submit" class="btn btn-success btn-circle btn-sm ml-2">
+        <button x-show="edit" x-on:click="edit = false" wire:click="submit" class="btn btn-success btn-circle btn-sm ml-2">
             <i class="fa fa-check"></i>
         </button>
         <a x-show="edit" x-on:click="edit = false" href="javascript:void(0)" class="btn btn-danger btn-circle btn-sm ml-2">
             <i class="fa fa-times"></i>
         </a>
     </td>
-    </form>
 </tr>
