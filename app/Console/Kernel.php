2cfcb93762ce130aac33ed67a8ac7cd32185fd71
stefuuuuu <stephan.sutter2000@gmail.com>
2024-08-25 21:51:52 +0800

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('queue:work --daemon')->everyMinute()->withoutOverlapping();
+        $schedule->command('queue:work --stop-when-empty')->everyMinute()->withoutOverlapping();
     }
 
     /**
